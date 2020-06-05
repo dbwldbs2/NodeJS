10e7aa86046d0467a4bdfcbbfc3602654f5fa897
dbwldbs2 <ahll011@naver.com>
2020-06-05 15:55:38 +0900

    2020/06/05

--- WebSocketProject/app.js
+++ WebSocketProject/app.js
@@ -3,16 +3,28 @@ const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const ColorHash = require('color-hash');
 const webSocket = require('./socket');
 const flash = require('connect-flash');
+
 
 require('dotenv').config();
 
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
+connect();
 
-
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    }
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,16 +35,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false
-    }
-}));
+app.use(sessionMiddleware);
 app.use(flash());
+
+app.use((req, res, next) => {
+    if(!req.session.color) {
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+    }
+    next();
+})
 
 app.use('/', indexRouter);
 
@@ -53,4 +65,4 @@ const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
--- WebSocketProject/socket.js
+++ WebSocketProject/socket.js
@@ -1,36 +1,61 @@
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, {path: '/socket.io'});
+    app.set('io', io);
+    
+    const room = io.of('./room');
+    const chat = io.of('./chat');
+    io.use((socket, next) => {
+        sessionMiddleware(socket.request, socket.request.res, next);
+    });
 
-    io.on('connection', (socket) => {
-        const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-        console.log('새로운 클라이언트 접속!');
-        console.log('ip :: ', ip);
-        console.log('socket.id :: ', socket.id);
-        console.log('req.ip :: ', req.ip);
+    room.on('connection', (socket) => {
+        console.log('room 네임 스페이스에 접속');
 
         socket.on('disconnect', () => {
-            console.log('클라이언트 접속 해제!');
-            console.log('ip :: ', ip);
-            console.log('socket.id :: ', socket.id);
-            console.log('req.ip :: ', req.ip);
-
-            clearInterval(socket.interval);
+            console.log('room 네임스페이스 접속 해제');
         });
-
-        socket.on('error', (error) => {
-            console.error(error);
-        });
-
-        socket.on('reply', (data) => {
-            console.log(data);
-        });
-
-        socket.interval = setInterval(() => {
-            socket.emit('news', 'Hello Socket.IO');
-        }, 3000);
     });
-}
+
+    chat.on('connection', (socket) => {
+        console.log('chat 네임 스페이스에 접속');
+
+        const req = socket.request;
+        const {headers: {referer}} = req;
+
+        console.log('referer :: ', referer);
+        
+        const roomId = referer
+            .split('/')[referer.split('/').length -1]
+            .replace(/\?.+/, '');
+        
+        socket.join(roomId);
+        socket.to(roomId).emdi('join', {
+            user: 'system',
+            chat: `${req.session.color}님이 입장하셨습니다.`
+        });
+
+        socket.on('disconnect', () => {
+            console.log('chat 네임스페이스 접속 해제');
+            socket.leave(roomId);
+            
+            const currentRoom = socket.adapter.room[roomId];
+            const userCount = currentRoom ? currentRoom.length : 0;
+            if (userCount === 0) {
+                axios.delete(`http://localhost:8005/room/${roomId}`)
+                    .then(() => {
+                        console.log('방 제거 요청 성공');
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } else {
+                socket.to(roomId).emit('exit', {
+                    user: 'system',
+                    chat: `${req.session.color}님이 퇴장하셨습니다.`
+                });
+            }
+        });
+    });
+}; 
